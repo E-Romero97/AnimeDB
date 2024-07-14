db0c73f513c6ce8ac2492f32276ca63f4a8c06b2
ErinsonRomero <118914142+ErinsonRomero@users.noreply.github.com>
2024-07-14 13:55:27 -0500

    feat: create new peoject

---
+++ AnimeDB/AnimeDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeDB
+//
+//  Created by Erinson Andres Romero Terry on 14/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
